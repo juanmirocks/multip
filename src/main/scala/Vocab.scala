ebad94bf0a7d0bf93627cf9e3c5df7632fe2de4f
Juan Miguel Cejuela <juanmi@jmcejuela.com>
2015-08-04 16:07:12 +0200

    simplify & improve efficiency (avoid unnecessary extra hash lookups)

--- src/main/scala/Vocab.scala
+++ src/main/scala/Vocab.scala
@@ -4,59 +4,56 @@ import scala.collection.mutable.HashMap
 import scala.collection.mutable.HashSet
 
 // Vocabulary class maps strings to integers for efficiency
-class Vocab(minCount:Int) {
-  def this() = this(1)
+class Vocab(minCount: Int = 1) {
 
   var string2Int   = new HashMap[String, Int]
   var string2Count = new HashMap[String, Int]
   var int2String   = new HashMap[Int, String]
-  val unk = -1
-  var nextInt = 0
-  var locked = false
-  
-  def apply(s:String):Int = {
-    var str = s
+  private val unk = -1
+  private var nextInt = 0
+  private var locked = false
 
-    //if(!string2Int.contains(str) && locked) {
-    if((!string2Int.contains(str) || string2Count(str) < minCount) && locked) {
+  def apply(s: String): Int = {
+
+    //if(!string2Int.contains(s) && locked) {
+    if((!string2Int.contains(s) || string2Count(s) < minCount) && locked) {
       return -1  //UNK is -1
     }
-    else if(!string2Int.contains(str)) {
-      string2Int   += str -> nextInt
-      int2String   += nextInt -> str
-      string2Count += str -> 0
+    else if(!string2Int.contains(s)) {
+      string2Int   += s -> nextInt
+      int2String   += nextInt -> s
+      string2Count += s -> 0
       nextInt += 1
     }
-    string2Count(str) += 1
-    return string2Int(str)
+    string2Count(s) += 1
+    return string2Int(s)
   }
 
-  def apply(i:Int):String = {
-    if(!int2String.contains(i)) {
-      "-UNK-"
-    } else {
-      int2String(i)
+  def apply(i: Int): String = {
+    int2String.get(i) match {
+      case None => "-UNK-"
+      case Some(x) => x
     }
   }
 
-  def lock = { 
+  def lock: Vocab = {
     locked = true
     this
   }
 
-  def getMinCountVocab = { 
+  def getMinCountVocab = {
     //locked = true
 
     val newVocab = new Vocab(minCount)
-    for((word, count) <- string2Count) {
-      if(count >= minCount) {
-	newVocab(word)
-	newVocab.string2Count(word) = count
+    for ((word, count) <- string2Count) {
+      if (count >= minCount) {
+        newVocab(word)
+        newVocab.string2Count(word) = count
       }
     }
 
     newVocab.locked = true
-    
+
     //this
     newVocab
   }
